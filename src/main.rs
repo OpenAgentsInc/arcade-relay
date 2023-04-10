a4d01fee82e6fbc0675aba5a53b14940810f2e0c
Christopher David <chris@arcadelabs.co>
2023-04-10 17:30:21 -0400

    i dont know - before axe that

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use hyper::server::Server;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{upgrade, Body, Request, Response};
+use hyper::{upgrade, Body, Request as HyperRequest, Response};
 use std::convert::Infallible;
 use std::convert::TryFrom;
+use tokio_tungstenite::tungstenite::handshake::server::Request as TungsteniteRequest;
 use tokio_tungstenite::WebSocketStream;
 use tracing::{info, Level};
 
@@ -24,34 +25,62 @@ async fn main() {
     }
 }
 
-async fn handle_request(mut request: Request<Body>) -> Result<Response<Body>, Infallible> {
-    match tokio_tungstenite::tungstenite::handshake::server::Request::<Body>::try_from(
-        request.clone(),
-    ) {
-        Ok(ws_req) => {
-            let response = ws_req.create_response();
-            tokio::spawn(async move {
-                match upgrade::on(&mut request).await {
-                    Ok(upgraded) => {
-                        let ws_stream = WebSocketStream::from_raw_socket(
-                            upgraded,
-                            tokio_tungstenite::tungstenite::protocol::Role::Server,
-                            None,
-                        )
-                        .await;
-                        // Handle WebSocket stream here.
+async fn handle_request(mut request: HyperRequest<Body>) -> Result<Response<Body>, Infallible> {
+    let mut headers = [httparse::EMPTY_HEADER; 64];
+    let mut req = httparse::Request::new(&mut headers);
+
+    if let Ok(_) = req.parse(request.headers().iter().map(|(name, value)| {
+        (
+            name.as_str(),
+            value.to_str().expect("header value should be valid"),
+        )
+    })) {
+        let tungstenite_req = TungsteniteRequest::from_headers(
+            req.method.unwrap(),
+            req.path.unwrap(),
+            req.version.unwrap(),
+            req.headers,
+        )
+        .unwrap();
+
+        match TungsteniteRequest::try_from(tungstenite_req) {
+            Ok(ws_req) => {
+                let response = create_response(&ws_req);
+                tokio::spawn(async move {
+                    match upgrade::on(&mut request).await {
+                        Ok(upgraded) => {
+                            let ws_stream = WebSocketStream::from_raw_socket(
+                                upgraded,
+                                tokio_tungstenite::tungstenite::protocol::Role::Server,
+                                None,
+                            )
+                            .await;
+                            // Handle WebSocket stream here.
+                        }
+                        Err(e) => eprintln!("Error upgrading connection: {}", e),
                     }
-                    Err(e) => eprintln!("Error upgrading connection: {}", e),
-                }
-            });
-            Ok(response)
+                });
+                Ok(response)
+            }
+            Err(_) => {
+                let response = Response::builder()
+                    .status(400)
+                    .body(Body::from("Invalid request"))
+                    .unwrap();
+                Ok(response)
+            }
         }
-        Err(_) => {
-            let response = Response::builder()
-                .status(400)
-                .body(Body::from("Invalid request"))
-                .unwrap();
-            Ok(response)
-        }
+    } else {
+        let response = Response::builder()
+            .status(400)
+            .body(Body::from("Invalid request"))
+            .unwrap();
+        Ok(response)
     }
+}
+
+fn create_response(
+    ws_req: &tokio_tungstenite::tungstenite::handshake::server::Request,
+) -> Response<Body> {
+    ws_req.accept().unwrap().finish().unwrap()
 }
